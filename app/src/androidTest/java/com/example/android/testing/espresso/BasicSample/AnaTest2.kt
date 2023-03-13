9b476f6edac8b026006799f5197de754efa4696f
AnaMandzulashvili <a.mandzulashvili@gmail.com>
2023-03-14 01:42:40 +0400

    Recorded_Espresso_Test

--- app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/AnaTest2.kt
+++ app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/AnaTest2.kt
@@ -1,37 +1,23 @@
 package com.example.android.testing.espresso.BasicSample
 
 
-import androidx.test.espresso.DataInteraction
-import androidx.test.espresso.ViewInteraction
-import androidx.test.filters.LargeTest
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-
-import androidx.test.InstrumentationRegistry.getInstrumentation
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
-
-import com.example.android.testing.espresso.BasicSample.R
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.anything
-import org.hamcrest.Matchers.`is`
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -44,35 +30,53 @@ class AnaTest2 {
     @Test
     fun anaTest2() {
         val editText = onView(
-allOf(withId(R.id.editTextUserInput),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-1),
-isDisplayed()))
+            allOf(
+                withId(R.id.editTextUserInput),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         editText.perform(replaceText("ana"), closeSoftKeyboard())
-        
+
         val button = onView(
-allOf(withId(R.id.activityChangeTextBtn), withText("Open activity and change text"),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-3),
-isDisplayed()))
+            allOf(
+                withId(R.id.activityChangeTextBtn), withText("Open activity and change text"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
         button.perform(click())
-        
+
         val textView = onView(
-allOf(withId(R.id.show_text_view), withText("ana"),
-withParent(allOf(withId(android.R.id.content),
-withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java)))),
-isDisplayed()))
+            allOf(
+                withId(R.id.show_text_view), withText("ana"),
+                withParent(
+                    allOf(
+                        withId(android.R.id.content),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
         textView.check(matches(withText("ana")))
-        }
-    
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -87,4 +91,4 @@ isDisplayed()))
             }
         }
     }
-    }
+}
